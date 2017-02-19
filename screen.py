26d34a11550a0c9a2bb26e8125433b202547043a
weirdgiraffe <giraffe@cyberzoo.xyz>
2017-02-19 11:14:12 +0100

    add week screen

--- screen.py
+++ screen.py
@@ -7,13 +7,28 @@
 #
 from kodi import plugin, logger
 from seasonvar import requester
+from datetime import datetime, timedelta
+
+
+def week():
+    date = datetime.today()
+    for date_offset in range(7):
+        datestr = date.strftime('%d.%m.%Y')
+        dayurl = plugin.make_url({'screen': 'day', 'date': datestr})
+        plugin.add_screen_directory(datestr, dayurl)
+        date -= timedelta(days=1)
+
+    searchurl = plugin.make_url({'screen': 'search'})
+    plugin.add_screen_directory('[COLOR FFFFD700]поиск[/COLOR]', searchurl)
+    plugin.publish_screen()
+
 
 if __name__ == "__main__":
     screen = plugin.args.get('screen')
     if screen is not None:
         try:
-            {'week': None,
-             'date': None,
+            {'week': week,
+             'day': None,
              'episodes': None,
              'seasons': None,
              'translations': None,
