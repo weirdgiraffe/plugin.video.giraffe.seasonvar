8d0c0c305e2a9afc5a53c1b0d9c33a77540f93d5
fg0g96 <nab@nab.by>
2017-05-31 00:46:04 +0300

    Fix typo

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import xbmcaddon
 
 a = xbmcaddon.Addon()
 p = a.getAddonInfo('path')
-sys.path.intsert(1, os.path.join(p, 'resources', 'site-packages'))
+sys.path.insert(1, os.path.join(p, 'resources', 'site-packages'))
 
 
 if __name__ == '__main__':
